dfc8a71f32bdcf99fe07cea254c45b38a5d1b1f4
Sayf-Eddine-El <s.elmessbahi@edu.umi.ac.ma>
2025-03-22 23:19:38 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,459 @@
+import { useState } from "react";
+import { playingCard, typesValue } from "./Cards/playingCard.js";
+import { useEffect } from "react";
+
+function App() {
+  const [deck, setDeck] = useState([]);
+  const [hand, setHand] = useState([]);
+  const [numberOfCards, setNumberOfCards] = useState(8);
+  const [currentHandType, setCurrentHandType] = useState("");
+  const [score, setScore] = useState(0);
+  const [plays, setPlays] = useState();
+  const [chipMult, setChipMult] = useState({
+    chip: 0,
+    multiplayer: 0,
+  });
+  const [currentBlind, setCurrentBlind] = useState(300);
+  const [roundeWin, setRoundWin] = useState(false);
+  const [gameState, setGameState] = useState(null);
+
+  useEffect(() => {
+    initializeDeck();
+    setPlays({
+      hands: 4,
+      discards: 4,
+    });
+    setNumberOfCards(8);
+  }, [gameState]);
+
+  useEffect(() => {
+    if (score > currentBlind) {
+      setRoundWin(true);
+    }
+    if (plays?.hands == 0) {
+      setRoundWin("Lose");
+    }
+  }, [hand]);
+
+  const initializeDeck = () => {
+    let cards = playingCard;
+    const shuffledDeck = cards.map(() => {
+      const randomIndex = Math.floor(Math.random() * (cards.length - 1));
+      const randomCard = cards[randomIndex];
+      cards = cards.filter((_, i) => i != randomIndex);
+      return { ...randomCard, active: false, discard: false };
+    });
+
+    setDeck(shuffledDeck);
+    setHand([]);
+    setCurrentHandType("");
+    if (gameState == true) {
+      setCurrentBlind((prev) => prev + 300);
+    }
+    setScore(0);
+
+    setChipMult({
+      chip: 0,
+      multiplayer: 0,
+    });
+    setRoundWin(false);
+    setGameState(null);
+  };
+
+  console.log(gameState);
+
+  const addCardToHand = (card) => {
+    if (hand.length < 5 && !hand.includes(card)) {
+      const currentDeck = [...deck];
+      const cardIndex = currentDeck.indexOf(card);
+      currentDeck[cardIndex].active = true;
+      setDeck(currentDeck);
+      setHand((prev) => [...prev, card]);
+    } else if (hand.includes(card)) {
+      const currentDeck = [...deck];
+      const cardIndex = currentDeck.indexOf(card);
+      currentDeck[cardIndex].active = false;
+      setDeck(currentDeck);
+      setHand((prev) => prev.filter((e, i) => i != hand.indexOf(card)));
+    }
+  };
+
+  const handleDiscardCards = (type) => {
+    if (plays.discards > 0) {
+      const currentDeck = [...deck];
+      for (let i = 0; i < hand.length; i++) {
+        const cardIndex = currentDeck.indexOf(hand[i]);
+        currentDeck[cardIndex].discard = true;
+        setNumberOfCards((prev) => prev + 1);
+      }
+      setHand([]);
+      setDeck(currentDeck);
+      if (type == "discard")
+        setPlays((prev) => {
+          return {
+            ...prev,
+            discards: prev.discards - 1,
+          };
+        });
+    }
+  };
+
+  const handleCalcScore = (type, cards) => {
+    const verified = ["Flush", "Straight", "Full house", "Straight flush"];
+    const notVerified = [
+      "Pair",
+      "Two Pair",
+      "Three of a kind",
+      "Four of a kind",
+    ];
+    if (verified.includes(type)) {
+      const handChips = hand.reduce((prev, curr) => prev + curr.chip, 0);
+      typesValue.map((e) => {
+        if (e.type == type) {
+          setScore((prev) => (handChips + e.chip) * e.multiplayer + prev);
+          setChipMult({
+            chip: e.chip + handChips,
+            multiplayer: e.multiplayer,
+          });
+        }
+      });
+    } else {
+      if (type == "High Card") {
+        let max = hand[0].chip;
+        hand.map((card) => {
+          if (card.chip > max) {
+            max = card.chip;
+          }
+        });
+        setScore((prev) => max + 5 + prev);
+        setChipMult({
+          chip: 5 + max,
+          multiplayer: 1,
+        });
+      }
+      if (notVerified.includes(type)) {
+        const cardsArray = [...Array(Object.keys(cards).length)].map((e, i) => {
+          return {
+            card: Object.keys(cards)[i],
+            selected: Object.values(cards)[i],
+          };
+        });
+
+        let max = cardsArray[0];
+
+        cardsArray.map((card) => {
+          if (max.selected < card.selected) {
+            max = card;
+          }
+        });
+
+        max = Number(Object.values(max)[0]);
+
+        let chips = 0;
+        for (let i = 0; i < hand.length; i++) {
+          if (hand[i].number == Number(max)) {
+            chips += hand[i].chip;
+          }
+        }
+        typesValue.map((e) => {
+          if (e.type == type) {
+            setScore((prev) => (chips + e.chip) * e.multiplayer + prev);
+            setChipMult({
+              chip: e.chip + chips,
+              multiplayer: e.multiplayer,
+            });
+          }
+        });
+      }
+    }
+  };
+
+  const handleCheckHand = () => {
+    if (checkStraight() && checkFlush()) {
+      setCurrentHandType("Straight Flush");
+      handleCalcScore("Straight flush");
+      handleDiscardCards("play");
+      setHand([]);
+      setPlays((prev) => {
+        return {
+          ...prev,
+          hands: prev.hands - 1,
+        };
+      });
+    } else if (checkFlush()) {
+      setCurrentHandType("Flush");
+      handleCalcScore("Flush");
+      handleDiscardCards();
+      setHand([]);
+      setPlays((prev) => {
+        return {
+          ...prev,
+          hands: prev.hands - 1,
+        };
+      });
+    } else if (checkStraight()) {
+      setCurrentHandType("Straight");
+      handleCalcScore("Straight");
+      handleDiscardCards();
+      setHand([]);
+      setPlays((prev) => {
+        return {
+          ...prev,
+          hands: prev.hands - 1,
+        };
+      });
+    } else if (checkPair().type == "Pair") {
+      setCurrentHandType("Pair");
+      handleCalcScore("Pair", checkPair().cards);
+      handleDiscardCards();
+      setHand([]);
+      setPlays((prev) => {
+        return {
+          ...prev,
+          hands: prev.hands - 1,
+        };
+      });
+    } else if (checkPair().type == "ThreeOfKind") {
+      setCurrentHandType("Three Of Kind");
+      handleCalcScore("Three of a kind", checkPair().cards);
+      handleDiscardCards();
+      setHand([]);
+      setPlays((prev) => {
+        return {
+          ...prev,
+          hands: prev.hands - 1,
+        };
+      });
+    } else if (checkPair().type == "FourOfKind") {
+      setCurrentHandType("Four Of Kind");
+      handleCalcScore("Four Of a kind", checkPair().cards);
+      handleDiscardCards();
+      setHand([]);
+      setPlays((prev) => {
+        return {
+          ...prev,
+          hands: prev.hands - 1,
+        };
+      });
+    } else if (checkPair().type == "TwoPair") {
+      setCurrentHandType("Two Pair");
+      handleCalcScore("Two Pair", checkPair().cards);
+      handleDiscardCards();
+      setHand([]);
+      setPlays((prev) => {
+        return {
+          ...prev,
+          hands: prev.hands - 1,
+        };
+      });
+    } else if (checkPair() == "FullHouse") {
+      setCurrentHandType("Full House");
+      handleCalcScore("Full house");
+      handleDiscardCards();
+      setHand([]);
+      setPlays((prev) => {
+        return {
+          ...prev,
+          hands: prev.hands - 1,
+        };
+      });
+    } else {
+      setCurrentHandType("High Card");
+      handleCalcScore("High Card");
+      handleDiscardCards();
+      setHand([]);
+      setPlays((prev) => {
+        return {
+          ...prev,
+          hands: prev.hands - 1,
+        };
+      });
+    }
+  };
+
+  const checkFlush = () => {
+    if (hand.length == 5) {
+      let isFlush = [];
+      for (let i = 0; i < hand.length; i++) {
+        if (isFlush[0] == undefined) {
+          isFlush = [hand[i].type];
+        } else {
+          if (isFlush[i - 1] == hand[i].type) {
+            isFlush = [...isFlush, hand[i].type];
+          } else {
+            return false;
+          }
+        }
+      }
+      return true;
+    }
+    return false;
+  };
+  const checkStraight = () => {
+    if (hand.length == 5) {
+      const sortedHand = [...hand].sort((a, b) => a.number - b.number);
+
+      let isStraight = [];
+      for (let i = 0; i < hand.length; i++) {
+        if (isStraight[0] == undefined) {
+          isStraight = [sortedHand[i].number];
+        } else {
+          if (isStraight[i - 1] + 1 == sortedHand[i].number) {
+            isStraight = [...isStraight, sortedHand[i].number];
+          } else {
+            return false;
+          }
+        }
+      }
+      return true;
+    }
+    return false;
+  };
+  const checkPair = () => {
+    if (hand.length >= 2) {
+      let isPair = {};
+
+      for (let i = 0; i < hand.length; i++) {
+        if (Object.keys(isPair).length == 0) {
+          isPair = {
+            [Number(hand[i].number)]: 1,
+          };
+        } else {
+          isPair = {
+            ...isPair,
+            [Number(hand[i].number)]: isPair[[Number(hand[i].number)]]
+              ? isPair[[Number(hand[i].number)]] + 1
+              : 1,
+          };
+        }
+      }
+      const max = Math.max(...Object.values(isPair));
+      const min = Math.min(...Object.values(isPair));
+      if (max == 2) {
+        if (
+          Object.values(isPair).includes(
+            max,
+            Object.values(isPair).indexOf(max) + 1
+          )
+        )
+          return {
+            type: "TwoPair",
+            cards: isPair,
+          };
+        else {
+          return {
+            type: "Pair",
+            cards: isPair,
+          };
+        }
+      }
+      if (max == 3 && min != 2) {
+        return { type: "ThreeOfKind", cards: isPair };
+      } else if (max == 3 && min == 2) {
+        return { type: "FullHouse", cards: isPair };
+      }
+      if (max == 4) {
+        return { type: "FourOfKind", cards: isPair };
+      }
+    }
+    return false;
+  };
+
+  return (
+    <div className="flex flex-col justify-center gap-10 items-center">
+      <h1 className="text-center font-bold p-4 text-3xl">Balatro</h1>
+      {roundeWin == true ? (
+        <div className="flex flex-row gap-8 items-center justify-center">
+          <h1 className="text-green-600 text-2xl font-bold">You won !</h1>
+          <button
+            onClick={() => setGameState(true)}
+            className="border-2 border-black rounded-sm p-2 font-bold cursor-pointer transition-all hover:scale-105"
+          >
+            Next Round
+          </button>
+        </div>
+      ) : roundeWin == "Lose" ? (
+        <div>
+          <h1 className="text-red-600 text-2xl font-bold">You Lose !</h1>
+          <button
+            onClick={() => setGameState(false)}
+            className="border-2 border-black rounded-sm p-2 font-bold cursor-pointer transition-all hover:scale-105"
+          >
+            Restart
+          </button>
+        </div>
+      ) : null}
+      <div className="flex flex-row gap-14 mt-14 items-center">
+        <div className="border-4 border-black gap-12 p-2 w-96">
+          <div className="flex text-xl flex-row justify-evenly p-4">
+            <h1 className="font-bold">Blind</h1>
+            <h1>{currentBlind}</h1>
+          </div>
+          <div className="flex flex-row justify-evenly p-4">
+            <h1>Round Score</h1>
+            <h1>{score}</h1>
+          </div>
+          <h1 className="w-full text-2xl text-center mb-5 font-bold mt-3">
+            {currentHandType || "No card type selected"}
+          </h1>
+          <div className="flex flex-row gap-4 p-2 items-center">
+            <h1 className="p-2 text-white font-bold text-2xl rounded-md text-center bg-blue-600 w-1/2">
+              {chipMult?.chip}
+            </h1>
+            <span className="text-red">X</span>
+            <h1 className=" p-2 text-white font-bold text-2xl rounded-md text-center bg-red-600 w-1/2">
+              {chipMult?.multiplayer}
+            </h1>
+          </div>
+          <div className="flex  flex-row mt-4 justify-evenly gap-4">
+            <div className="flex w-1/2 flex-col border-2 border-black rounded-sm gap-4 justify-center items-center p-2">
+              <h1 className="text-center font-bold">Hands</h1>
+              <h1>{plays?.hands}</h1>
+            </div>
+            <div className="flex w-1/2 items-center justify-center flex-col border-2 border-black rounded-sm gap-4 p-2">
+              <h1 className="text-center font-bold">Discards</h1>
+              <h1>{plays?.discards}</h1>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col h-48 justify-between gap-8">
+          <div className=" flex flex-row gap-6">
+            {[...Array(numberOfCards)].map((e, i) =>
+              deck[i]?.discard == false ? (
+                <img
+                  className="border-4 scale-110 hover:scale-[1.15] max-h-[100px] cursor-pointer transition-all p-[1px] rounded-md border-black max-w-[69px]"
+                  src={deck[i]?.img}
+                  key={i}
+                  onClick={() => addCardToHand(deck[i])}
+                  style={{
+                    transform: deck[i]?.active
+                      ? "translateY(-20px)"
+                      : "translateY(0px)",
+                  }}
+                />
+              ) : null
+            )}
+          </div>
+          <div className="flex flex-row justify-center gap-4">
+            <button
+              onClick={handleCheckHand}
+              className=" rounded-sm font-bold bg-blue-500 transition-all text-white hover:scale-105 border-2 w-40 cursor-pointer border-black p-2"
+            >
+              Play
+            </button>
+            <button
+              onClick={() => handleDiscardCards("discard")}
+              className=" rounded-sm font-bold  transition-all text-white hover:scale-105 active:scale-110 border-2 w-40 cursor-pointer border-black p-2"
+              disabled={plays?.discards == 0 ? true : false}
+              style={{ backgroundColor: plays?.discards == 0 ? "gray" : "red" }}
+            >
+              Discard
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Cards/playingCard.js
@@ -0,0 +1,387 @@
+import bat_1 from "../maroc_card/1_bat.png";
+import bat_2 from "../maroc_card/2_bat.png";
+import bat_3 from "../maroc_card/3_bat.png";
+import bat_4 from "../maroc_card/4_bat.png";
+import bat_5 from "../maroc_card/5_bat.png";
+import bat_6 from "../maroc_card/6_bat.png";
+import bat_7 from "../maroc_card/7_bat.png";
+import bat_8 from "../maroc_card/8_bat.png";
+import bat_9 from "../maroc_card/9_bat.png";
+import bat_10 from "../maroc_card/10_bat.png";
+import bat_11 from "../maroc_card/11_bat.png";
+import bat_12 from "../maroc_card/12_bat.png";
+import coin_1 from "../maroc_card/1_coin.png";
+import coin_2 from "../maroc_card/2_coin.png";
+import coin_3 from "../maroc_card/3_coin.png";
+import coin_4 from "../maroc_card/4_coin.png";
+import coin_5 from "../maroc_card/5_coin.png";
+import coin_6 from "../maroc_card/6_coin.png";
+import coin_7 from "../maroc_card/7_coin.png";
+import coin_8 from "../maroc_card/8_coin.png";
+import coin_9 from "../maroc_card/9_coin.png";
+import coin_10 from "../maroc_card/10_coin.png";
+import coin_11 from "../maroc_card/11_coin.png";
+import coin_12 from "../maroc_card/12_coin.png";
+import sword_1 from "../maroc_card/1_sword.png";
+import sword_2 from "../maroc_card/2_sword.png";
+import sword_3 from "../maroc_card/3_sword.png";
+import sword_4 from "../maroc_card/4_sword.png";
+import sword_5 from "../maroc_card/5_sword.png";
+import sword_6 from "../maroc_card/6_sword.png";
+import sword_7 from "../maroc_card/7_sword.png";
+import sword_8 from "../maroc_card/8_sword.png";
+import sword_9 from "../maroc_card/9_sword.png";
+import sword_10 from "../maroc_card/10_sword.png";
+import sword_11 from "../maroc_card/11_sword.png";
+import sword_12 from "../maroc_card/12_sword.png";
+import plate_1 from "../maroc_card/1_plate.png";
+import plate_2 from "../maroc_card/2_plate.png";
+import plate_3 from "../maroc_card/3_plate.png";
+import plate_4 from "../maroc_card/4_plate.png";
+import plate_5 from "../maroc_card/5_plate.png";
+import plate_6 from "../maroc_card/6_plate.png";
+import plate_7 from "../maroc_card/7_plate.png";
+import plate_8 from "../maroc_card/8_plate.png";
+import plate_9 from "../maroc_card/9_plate.png";
+import plate_10 from "../maroc_card/10_plate.png";
+import plate_11 from "../maroc_card/11_plate.png";
+import plate_12 from "../maroc_card/12_plate.png";
+
+export const playingCard = [
+  {
+    number: 1,
+    type: "Gra3",
+    chip: 11,
+    img: bat_1,
+  },
+  {
+    number: 2,
+    type: "Gra3",
+    chip: 2,
+    img: bat_2,
+  },
+  {
+    number: 3,
+    type: "Gra3",
+    chip: 3,
+    img: bat_3,
+  },
+  {
+    number: 4,
+    type: "Gra3",
+    chip: 4,
+    img: bat_4,
+  },
+  {
+    number: 5,
+    type: "Gra3",
+    chip: 5,
+    img: bat_5,
+  },
+  {
+    number: 6,
+    type: "Gra3",
+    chip: 6,
+    img: bat_6,
+  },
+  {
+    number: 7,
+    type: "Gra3",
+    chip: 7,
+    img: bat_7,
+  },
+  {
+    number: 8,
+    type: "Gra3",
+    chip: 8,
+    img: bat_8,
+  },
+  {
+    number: 9,
+    type: "Gra3",
+    chip: 9,
+    img: bat_9,
+  },
+  {
+    number: 10,
+    type: "Gra3",
+    chip: 10,
+    img: bat_10,
+  },
+  {
+    number: 11,
+    type: "Gra3",
+    chip: 11,
+    img: bat_11,
+  },
+  {
+    number: 12,
+    type: "Gra3",
+    chip: 12,
+    img: bat_12,
+  },
+  {
+    number: 1,
+    type: "Coin",
+    chip: 11,
+    img: coin_1,
+  },
+  {
+    number: 2,
+    type: "Coin",
+    chip: 2,
+    img: coin_2,
+  },
+  {
+    number: 3,
+    type: "Coin",
+    chip: 3,
+    img: coin_3,
+  },
+  {
+    number: 4,
+    type: "Coin",
+    chip: 4,
+    img: coin_4,
+  },
+  {
+    number: 5,
+    type: "Coin",
+    chip: 5,
+    img: coin_5,
+  },
+  {
+    number: 6,
+    type: "Coin",
+    chip: 6,
+    img: coin_6,
+  },
+  {
+    number: 7,
+    type: "Coin",
+    chip: 7,
+    img: coin_7,
+  },
+  {
+    number: 8,
+    type: "Coin",
+    chip: 8,
+    img: coin_8,
+  },
+  {
+    number: 9,
+    type: "Coin",
+    chip: 9,
+    img: coin_9,
+  },
+  {
+    number: 10,
+    type: "Coin",
+    chip: 10,
+    img: coin_10,
+  },
+  {
+    number: 11,
+    type: "Coin",
+    chip: 11,
+    img: coin_11,
+  },
+  {
+    number: 12,
+    type: "Coin",
+    chip: 12,
+    img: coin_12,
+  },
+  {
+    number: 1,
+    type: "sword",
+    chip: 11,
+    img: sword_1,
+  },
+  {
+    number: 2,
+    type: "sword",
+    chip: 2,
+    img: sword_2,
+  },
+  {
+    number: 3,
+    type: "sword",
+    chip: 3,
+    img: sword_3,
+  },
+  {
+    number: 4,
+    type: "sword",
+    chip: 4,
+    img: sword_4,
+  },
+  {
+    number: 5,
+    type: "sword",
+    chip: 5,
+    img: sword_5,
+  },
+  {
+    number: 6,
+    type: "sword",
+    chip: 6,
+    img: sword_6,
+  },
+  {
+    number: 7,
+    type: "sword",
+    chip: 7,
+    img: sword_7,
+  },
+  {
+    number: 8,
+    type: "sword",
+    chip: 8,
+    img: sword_8,
+  },
+  {
+    number: 9,
+    type: "sword",
+    chip: 9,
+    img: sword_9,
+  },
+  {
+    number: 10,
+    type: "sword",
+    chip: 10,
+    img: sword_10,
+  },
+  {
+    number: 11,
+    type: "sword",
+    chip: 11,
+    img: sword_11,
+  },
+  {
+    number: 12,
+    type: "sword",
+    chip: 12,
+    img: sword_12,
+  },
+  {
+    number: 1,
+    type: "plate",
+    chip: 11,
+    img: plate_1,
+  },
+  {
+    number: 2,
+    type: "plate",
+    chip: 2,
+    img: plate_2,
+  },
+  {
+    number: 3,
+    type: "plate",
+    chip: 3,
+    img: plate_3,
+  },
+  {
+    number: 4,
+    type: "plate",
+    chip: 4,
+    img: plate_4,
+  },
+  {
+    number: 5,
+    type: "plate",
+    chip: 5,
+    img: plate_5,
+  },
+  {
+    number: 6,
+    type: "plate",
+    chip: 6,
+    img: plate_6,
+  },
+  {
+    number: 7,
+    type: "plate",
+    chip: 7,
+    img: plate_7,
+  },
+  {
+    number: 8,
+    type: "plate",
+    chip: 8,
+    img: plate_8,
+  },
+  {
+    number: 9,
+    type: "plate",
+    chip: 9,
+    img: plate_9,
+  },
+  {
+    number: 10,
+    type: "plate",
+    chip: 10,
+    img: plate_10,
+  },
+  {
+    number: 11,
+    type: "plate",
+    chip: 11,
+    img: plate_11,
+  },
+  {
+    number: 12,
+    type: "plate",
+    chip: 12,
+    img: plate_12,
+  },
+];
+
+export const typesValue = [
+  {
+    type: "Straight flush",
+    multiplayer: 8,
+    chip: 100,
+  },
+  {
+    type: "Four Of a kind",
+    multiplayer: 7,
+    chip: 60,
+  },
+  {
+    type: "Full house",
+    multiplayer: 4,
+    chip: 40,
+  },
+  {
+    type: "Flush",
+    multiplayer: 4,
+    chip: 35,
+  },
+  {
+    type: "Straight",
+    multiplayer: 4,
+    chip: 30,
+  },
+  {
+    type: "Three of a kind",
+    multiplayer: 4,
+    chip: 30,
+  },
+  {
+    type: "Two Pair",
+    multiplayer: 2,
+    chip: 20,
+  },
+  {
+    type: "Pair",
+    multiplayer: 2,
+    chip: 10,
+  },
+  {
+    type: "High Card",
+    multiplayer: 1,
+    chip: 5,
+  },
+];
